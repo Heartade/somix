8deaf41aa50d4c8e3c9984136657c77093d3277f
Isaac Beverly <imbev@protonmail.com>
2022-11-21 15:03:53 -0600

    Add skeleton of main page and navbar

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,31 @@
+mod components;
+use components::atoms::nav::Nav;
+
 use yew::prelude::*;
 
 #[function_component(App)]
 pub fn app() -> Html {
     html! {
         <div>
-            {"matrix-social"}
+            <Nav></Nav>
+            <br/>
+            <div class="columns has-text-centered is-centered">
+                <div class="column is-two-fifths">
+                    <div class="box has-background-dark">
+                        <p class="subtitle has-text-primary">
+                            {"Lorem ipsum dolor sit amet"}
+                        </p>
+                    </div>
+                    <div class="box has-background-dark">
+                        <p class="subtitle has-text-primary">
+                            {"consectetur adipiscing elit"}
+                        </p>
+                    </div>
+                </div>
+                <div class="column is-one-fifth">
+                    <p class="box has-background-dark has-text-primary">{"Lorem ipsum"}</p>
+                </div>
+            </div>
         </div>
     }
 }
