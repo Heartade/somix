5ec68b824d131b3bfda2d4a6fbc70b92edffeb64
Isaac Beverly <imbev@protonmail.com>
2022-11-23 15:03:57 -0600

    Rewrite guest to run on first render automatically

--- src/components/atoms/guest.rs
+++ src/components/atoms/guest.rs
@@ -31,31 +31,10 @@ pub fn guest() -> Html {
 
     let state =
         use_async(async move { get_guest("https://matrix.org".to_owned(), &storage).await });
-    let onclick = {
+    
+    if use_is_first_mount() {
         let state = state.clone();
-        Callback::from(move |_| {
-            state.run();
-        })
-    };
-    html! {
-    <div>
-            {
-                if let Some(data) = &state.data {
-                    html! { data }
-                } else if state.loading {
-                    html! {<button class="button is-primary has-text-dark" disabled=true>{ "loading..." }</button>}
-                }
-                else {
-                    html! {<button {onclick} class="button is-primary has-text-dark">{ "Continue as guest" }</button>}
-                }
-            }
-            {
-                if let Some(error) = &state.error {
-                    html! { <button class="button is-danger has-text-dark" disabled=true>{ error }</button> }
-                } else {
-                    html! {}
-                }
-            }
-        </div>
-        }
+        state.run();
+    }
+    html! {}
 }
