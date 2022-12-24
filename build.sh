f89331da362e911fca51273f652e41e313400445
Isaac Beverly <imbev@protonmail.com>
2022-12-24 14:30:17 -0600

    Build with public url for pages CI

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 rustup toolchain install stable && rustup target add wasm32-unknown-unknown
 bash -cl "wget -qO- https://github.com/thedodd/trunk/releases/download/v0.16.0/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xzf- && mv ./trunk /usr/bin/"
-trunk build
+trunk build $@
