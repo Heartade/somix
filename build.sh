4400e091ac23a0b7f8282e3c44cb4706f5e15b13
Isaac Beverly <imbev@protonmail.com>
2022-12-24 13:52:47 -0600

    Replace dockerfile with shell script

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+rustup toolchain install stable && rustup target add wasm32-unknown-unknown
+bash -cl "wget -qO- https://github.com/thedodd/trunk/releases/download/v0.16.0/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xzf- && mv ./trunk /usr/bin/"
+trunk build
