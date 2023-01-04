31b7c14776cb7bc6cfdfcf3405ea098fc694ceb0
Isaac Beverly <imbev@protonmail.com>
2023-01-04 12:11:04 -0600

    Rewrite event view using tailwind

--- src/components/event.rs
+++ src/components/event.rs
@@ -57,32 +57,14 @@ pub fn event(props: &Props) -> Html {
         None => html! { format!{"Post \"{event_id}\" not found."} },
     };
     html! {
-       <>
-       <div class="columns has-text-centered is-centered">
-           <div class="column is-two-fifths">
-               { content }
-               <article class="media">
-               <div class="media-content">
-                 <div class="field">
-                   <p class="control">
-                     <textarea class="textarea has-background-dark has-text-primary" placeholder="Say something...">{"aaa"}</textarea>
-                   </p>
-                 </div>
-                 <nav class="level">
-                   <div class="level-left">
-                     <div class="level-item">
-                       <a class="button is-primary">{"Send"}</a>
-                     </div>
-                   </div>
-                 </nav>
-               </div>
-             </article>
-             { comments }
-           </div>
-           <div class="column is-one-fifth">
-               <p class="box has-background-dark has-text-primary">{"Placeholder"}</p>
-           </div>
+       <div class="flex sm:flex-nowrap justify-center">
+            <div class="w-max md:w-2/3 lg:w-1/2 xl:w-2/5 2xl:w-1/3 mx-4">
+                { content }
+                <div class="flex justify-center w-full h-46 gap-4 py-4 px-8 mb-4 border border-tuatara-400 rounded bg-tuatara-700">
+                    <span class="sticky text-center text-charm-400">{"Placeholder"}</span>
+                </div>
+                { comments }
+            </div>
        </div>
-       </>
     }
 }
