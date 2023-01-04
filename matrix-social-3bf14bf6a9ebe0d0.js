f67ba4858baab5506eaf59c161971ef33d61ee1b
Woodpecker CI <noreply@example.org>
2023-01-04 14:21:51 +0000

    Woodpecker CI 1672841896

--- matrix-social-3bf14bf6a9ebe0d0.js
+++ matrix-social-3bf14bf6a9ebe0d0.js
@@ -958,7 +958,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('matrix-social-205b390d5eb192b5_bg.wasm', import.meta.url);
+        input = new URL('matrix-social-3bf14bf6a9ebe0d0_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
