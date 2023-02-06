f1b38e3e375750011bca85774b83b9577ba3969f
Isaac Beverly <imbev@protonmail.com>
2023-02-06 11:34:10 -0600

    Disable reaction placeholder

--- src/components/post.rs
+++ src/components/post.rs
@@ -158,13 +158,15 @@ pub fn post(props: &Props) -> Html {
                         </svg>
                         <span class="group-hover:text-charm-300">{post.reply_ids.clone().len()}{" Comment(s)"}</span>
                     </Link<Route>>
+                    /*
                     <button class="flex gap-1 hover:bg-tuatara-500 p-2 rounded group">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                              class="w-6 h-6 group-hover:stroke-charm-300">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z" />
                         </svg>
                         <span class="group-hover:text-charm-300">{"React"}</span>
-                    </button>
+                    </button> 
+                    */
                     {
                         if props.show_return_button {
                             match post.reply_to.clone() {
